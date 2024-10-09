9939fe9f0e4ca79084ac92dace2b7055fbbf9535
sdsd <dev.dionisiu.brovka@gmail.com>
2024-10-09 12:02:41 +0300

    Создано базовое приложение wagtail

---
+++ website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-0@xonw8d6cn-j%7-s8l+0@%j_2&#02aoi^s39=ouxlgl2h@6mq" #REMAKE
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
